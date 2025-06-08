aaa0dd1c4e151e99f7da96b76d16b712f1cad8cb
Woehammer <72220895+Woehammer@users.noreply.github.com>
2025-06-08 21:42:00 +0000

    Create battlefield_state.py

---
+++ battlefield_state.py
@@ -0,0 +1,21 @@
+class BattlefieldState:
+    def __init__(self):
+        self.killaboss_enemy_distance = 0
+        self.killaboss_objective_distance = 0
+        self.killaboss_charge = "No"
+
+        self.murknob_enemy_distance = 0
+        self.murknob_objective_distance = 0
+        self.murknob_charge = "No"
+
+        self.gutrippaz_enemy_distance = 0
+        self.gutrippaz_objective_distance = 0
+        self.gutrippaz_charge = "No"
+
+        self.boltboyz_enemy_distance = 0
+        self.boltboyz_objective_distance = 0
+        self.boltboyz_charge = "No"
+
+        self.killbow_enemy_distance = 0
+        self.killbow_objective_distance = 0
+        self.killbow_charge = "No"
